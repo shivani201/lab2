5b12ae77c29d72640f752fd3bbbb54d65f8e36d8
shivani201 <42140799+shivani201@users.noreply.github.com>
2018-08-13 15:59:18 +0530

    Add files via upload

---
+++ lab2_q2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "what is your name?"
+read NAME 
+echo "you are $NAME"
+mkdir $NAME
+cd $NAME
+touch $NAME1.cpp
+touch $NAME2.cpp
+touch $NAME3.cpp
+touch $NAME4.cpp
+touch $NAME5.cpp
